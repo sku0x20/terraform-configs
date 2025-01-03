4428ef52fa9b9bc42bc942aae38dd4f88bc4be37
siddhant <siddhantkumarupmanyu@gmail.com>
2025-01-03 17:25:28 +0530

    fix typo

--- image/data/install-tools.sh
+++ image/data/install-tools.sh
@@ -20,7 +20,7 @@ install_hashicorp_tools() {
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
     apt-get update
-    apt-get install -y nomad vault consul consol-template
+    apt-get install -y nomad vault consul consul-template
 }
 
 install_docker
