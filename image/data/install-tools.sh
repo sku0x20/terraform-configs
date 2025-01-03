9bea0e8dcdeb6b921c552d08446dc312e5de88ee
siddhant <siddhantkumarupmanyu@gmail.com>
2025-01-03 17:00:50 +0530

    remove sudo

--- image/data/install-tools.sh
+++ image/data/install-tools.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 install_docker() {
-    sudo apt-get update
-    sudo apt-get -y install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    apt-get update
+    apt-get install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    sudo apt-get update
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 install_hashicorp_tools() {
-    sudo apt-get update
-    sudo apt-get -y install wget gpg coreutils
+    apt-get update
+    apt-get -y install wget gpg coreutils
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-    sudo apt-get update
-    sudo apt-get -y install nomad
-    sudo apt-get -y install consul
-    sudo apt-get -y install vault
+    apt-get update
+    apt-get install -y nomad vault consul
 }
 
 install_docker
