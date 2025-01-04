03b925502028d2544631d5b559fce3b3756308e9
siddhant <siddhantkumarupmanyu@gmail.com>
2025-01-05 00:15:36 +0530

    remove docker build package

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -11,7 +11,7 @@ install_docker() {
     chmod a+r /etc/apt/keyrings/docker.asc
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
     apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
 
 install_hashicorp_tools() {
