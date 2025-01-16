3efa90eb391895efb05c5113c41ef9b19a988a84
siddhant <siddhantkumarupmanyu@gmail.com>
2025-01-17 03:19:26 +0530

    only install nomad

--- nomad/scripts/install-tools.sh
+++ nomad/scripts/install-tools.sh
@@ -20,7 +20,7 @@ install_hashicorp_tools() {
     wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
     apt-get update
-    apt-get install -y nomad vault consul consul-template
+    apt-get install -y nomad
 }
 
 install_docker
