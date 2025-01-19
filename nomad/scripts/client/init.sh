0e437d61f41eae2b4d6e442612c69e2f100a3dc0
siddhant <siddhantkumarupmanyu@gmail.com>
2025-01-19 17:18:37 +0530

    fix chmod in init

--- nomad/scripts/client/init.sh
+++ nomad/scripts/client/init.sh
@@ -3,5 +3,5 @@
 apt-get -y update
 apt-get -y install wget
 wget https://raw.githubusercontent.com/sku0x20/terraform-configs/refs/heads/main/nomad/scripts/client/install_tools.sh
-chmod +x install-tools.sh
+chmod +x install_tools.sh
 ./install-tools.sh
