48f206b85953074edea2d414adf1145c33bad727
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-22 16:19:06 +0530

    add describe

--- nomad-deployments/dummy/index.js
+++ nomad-deployments/dummy/index.js
@@ -1,8 +1,20 @@
 const server = Bun.serve({
     port: 3000,
+    routes: {
+        "/describe": describe,
+    },
     fetch: (req) => {
-        return new Response("from Bun!")
+        return new Response("not found", { status: 404 })
     }
 })
 
 console.log(`listening on http://localhost:${server.port}`)
+
+
+function describe(req) {
+    return Response.json({
+        "api": "/describe",
+        "name": `${process.env.APP_NAME}`,
+        "caller": `${req.headers.get("host")}`,
+    })
+}
