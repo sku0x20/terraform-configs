c4aa957646817504c8b3ec62b9a48d221c795213
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-22 15:59:01 +0530

    add bun.serve

--- nomad-deployments/dummy/index.js
+++ nomad-deployments/dummy/index.js
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch: (req) => {
+        return new Response("from Bun!")
+    }
+})
+
+console.log(`listening on http://localhost:${server.port}`)
