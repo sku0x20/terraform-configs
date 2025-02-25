7e990760e0f0701586cff0fb6f1f68185c7a2bc9
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-25 17:40:00 +0530

    add init scripts

---
+++ consul/scripts/client/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y install wget
+wget https://raw.githubusercontent.com/sku0x20/terraform-configs/refs/heads/main/consul/scripts/client/install_tools.sh
+chmod +x install_tools.sh
+./install_tools.sh
