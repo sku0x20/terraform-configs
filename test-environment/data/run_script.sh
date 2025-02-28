dbfc4297c147231d421e58b7495f60893545599d
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-28 22:38:57 +0530

    wip

--- test-environment/data/run_script.sh
+++ test-environment/data/run_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "hello from run_script.sh"
-cat some-data.txt
-mv some-data.txt /tmp/some-data-moved.txt
+# cat some-data.txt
+# mv some-data.txt /tmp/some-data-moved.txt
--- test-environment/init-data.tf
+++ test-environment/init-data.tf
@@ -2,13 +2,13 @@
 // https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/config
 // https://developer.hashicorp.com/terraform/language/resources/provisioners/syntax
 
-# data "cloudinit_config" "my_cloud_config" {
-#   gzip          = false
-#   base64_encode = false
-#   part {
-#     filename = "some-data.txt"
-#     content_type = ""
-#     content = "test"
-#   }
-# }
+data "cloudinit_config" "my_cloud_config" {
+  gzip          = false
+  base64_encode = false
+  part {
+    filename     = "run_script.sh"
+    content_type = "text/x-shellscript"
+    content      = file("${path.module}/data/run_script.sh")
+  }
+}
 
