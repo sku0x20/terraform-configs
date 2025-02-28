7ba7de5436d178f147914045110e5dd9594c38eb
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-28 23:30:04 +0530

    update script naming

--- consul/init-data.tf
+++ consul/init-data.tf
@@ -6,9 +6,9 @@ data "cloudinit_config" "server_cloud_config" {
   gzip          = false
   base64_encode = false
   part {
-    filename     = "init.sh"
+    filename     = "shim.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/scripts/server/init.sh")
+    content      = file("${path.module}/scripts/server/shim.sh")
   }
 
   // write files via cloud-config as multipath doesn't support writting plain files
@@ -19,19 +19,19 @@ data "cloudinit_config" "server_cloud_config" {
     content = yamlencode({
       write_files : [
         {
-          path : "/run/cloud-data/consul-agent-ca.pem"
+          path : "/run/cloud-data-consul/consul-agent-ca.pem"
           content : file("${path.module}/data/consul-agent-ca.pem")
         },
         {
-          path : "/run/cloud-data/dc1-server-consul-0-key.pem"
+          path : "/run/cloud-data-consul/dc1-server-consul-0-key.pem"
           content : file("${path.module}/data/dc1-server-consul-0-key.pem")
         },
         {
-          path : "/run/cloud-data/dc1-server-consul-0.pem"
+          path : "/run/cloud-data-consul/dc1-server-consul-0.pem"
           content : file("${path.module}/data/dc1-server-consul-0.pem")
         },
         {
-          path : "/run/cloud-data/keygen.txt"
+          path : "/run/cloud-data-consul/keygen.txt"
           content : file("${path.module}/data/keygen.txt")
         },
       ]
@@ -43,9 +43,9 @@ data "cloudinit_config" "client_cloud_config" {
   gzip          = false
   base64_encode = false
   part {
-    filename     = "init.sh"
+    filename     = "shim.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/scripts/client/init.sh")
+    content      = file("${path.module}/scripts/client/shim.sh")
   }
 
   // write files via cloud-config as multipath doesn't support writting plain files
@@ -56,11 +56,11 @@ data "cloudinit_config" "client_cloud_config" {
     content = yamlencode({
       write_files : [
         {
-          path : "/run/cloud-data/consul-agent-ca.pem"
+          path : "/run/cloud-data-consul/consul-agent-ca.pem"
           content : file("${path.module}/data/consul-agent-ca.pem")
         },
         {
-          path : "/run/cloud-data/keygen.txt"
+          path : "/run/cloud-data-consul/keygen.txt"
           content : file("${path.module}/data/keygen.txt")
         },
       ]
