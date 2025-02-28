3c7f0dfdd416a36d36e4e6acce7f870a06082931
siddhant <siddhantkumarupmanyu@gmail.com>
2025-02-28 23:30:47 +0530

    consul wip: update scripts

--- consul/scripts/client/shim.sh
+++ consul/scripts/client/shim.sh
@@ -2,6 +2,6 @@
 
 apt-get -y update
 apt-get -y install wget
-wget https://raw.githubusercontent.com/sku0x20/terraform-configs/refs/heads/main/consul/scripts/client/install_tools.sh
-chmod +x install_tools.sh
-./install_tools.sh
+wget https://raw.githubusercontent.com/sku0x20/terraform-configs/refs/heads/main/consul/scripts/client/main.sh
+chmod +x main.sh
+./main.sh
