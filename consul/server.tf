8ec62bcb2715267fad03e9366395b90dd260294e
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-04 12:41:53 +0530

    extract to clients and server

---
+++ consul/server.tf
@@ -0,0 +1,26 @@
+
+resource "aws_instance" "server" {
+  count = var.server_count
+
+  ami                    = var.ami
+  instance_type          = var.server_instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.server.id]
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.id
+  tags = {
+    Name                 = "${var.name}-server-${count.index}"
+    consul-instance-type = "server"
+  }
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = var.server_root_block_device_size
+    delete_on_termination = true
+  }
+
+  user_data = data.cloudinit_config.server_cloud_config.rendered
+}
+
