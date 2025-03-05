816584f80019a2ca3d9514a9e12a98fdc8a51145
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-05 15:17:33 +0530

    tidy outputs

--- nomad/outputs.tf
+++ nomad/outputs.tf
@@ -2,6 +2,14 @@ output "server_public_ips" {
   value = aws_instance.server[*].public_ip
 }
 
-output "server_ipv6" {
+output "server_ipv6_ips" {
   value = aws_instance.server[*].ipv6_addresses
+}
+
+output "client_public_ips" {
+  value = aws_instance.client[*].public_ip
+}
+
+output "client_ipv6_ips" {
+  value = aws_instance.client[*].ipv6_addresses
 }
