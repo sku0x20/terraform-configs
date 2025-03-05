00607bda85f145b4933cd73f81ccd4c80a171f29
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-05 15:16:58 +0530

    tidy variables

--- nomad/variabes.tf
+++ nomad/variabes.tf
@@ -1,3 +1,7 @@
+variable "name" {
+  default = "nomad"
+}
+
 variable "region" {
   default = "ap-south-1"
 }
@@ -6,30 +10,34 @@ variable "ami" {
   default = "ami-00bb0af6826df0a03"
 }
 
-variable "server_instance_type" {
-  default = "t3.micro"
-}
-
-variable "client_instance_type" {
-  default = "t3.micro"
-}
-
 variable "key_name" {
   default = "aws-key"
+}
+
+// -- server --
+
+variable "server_instance_type" {
+  default = "t3.micro"
 }
 
 variable "server_count" {
   default = 3
 }
 
+variable "server_root_block_device_size" {
+  default = 10
+}
+
+// -- client --
+
+variable "client_instance_type" {
+  default = "t3.micro"
+}
+
 variable "client_count" {
   default = 3
 }
 
-variable "name" {
-  default = "test"
-}
-
-variable "root_block_device_size" {
+variable "client_root_block_device_size" {
   default = 10
 }
