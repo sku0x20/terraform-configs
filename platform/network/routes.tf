f2a69bc7dbbd25bd68a9bac1d3724c7d29c6be96
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-06 18:15:21 +0530

    add public route for public subnet

--- platform/network/routes.tf
+++ platform/network/routes.tf
@@ -17,3 +17,21 @@ resource "aws_route_table_association" "nat-with-a" {
   subnet_id      = aws_subnet.a.id
   route_table_id = aws_route_table.nat.id
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    Name = "${var.name}-public"
+  }
+}
+
+resource "aws_route_table_association" "ig-with-public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
