740231af40e9ed1463c84c78ce526748ff99511f
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-06 14:51:32 +0530

    update subnet name

--- platform/network/subnets.tf
+++ platform/network/subnets.tf
@@ -1,12 +1,11 @@
 
-resource "aws_subnet" "a" {yes
-
+resource "aws_subnet" "a" {
   vpc_id                  = aws_vpc.platform.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.name}-subnet-1"
+    Name = "${var.name}-subnet-1a"
   }
 }
