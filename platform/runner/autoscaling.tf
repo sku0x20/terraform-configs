bffaa1cffdd62130b628b132b2184bcf50d1c492
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-07 13:55:17 +0530

    add autoscaling

--- platform/runner/autoscaling.tf
+++ platform/runner/autoscaling.tf
@@ -1,6 +1,20 @@
 
 
-# resource "aws_autoscaling_group" "scale" {
-#   min_size = 2
-#   max_size = 3
-# }
+resource "aws_autoscaling_group" "scale" {
+
+  launch_template {
+    id = aws_launch_template.template.id
+  }
+  vpc_zone_identifier = [var.subnet_id]
+
+  min_size = 3
+  max_size = 9
+
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+
+  instance_maintenance_policy {
+    min_healthy_percentage = 100
+    max_healthy_percentage = 120
+  }
+}
