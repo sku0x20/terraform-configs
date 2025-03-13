8e831164afada4a241be36760d03df987f301966
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-13 18:03:26 +0530

    update scaling policy name

--- platform/runner/autoscaling_policy.tf
+++ platform/runner/autoscaling_policy.tf
@@ -1,7 +1,7 @@
 
 
-resource "aws_autoscaling_policy" "scale" {
-  name                   = "manual-scaling"
+resource "aws_autoscaling_policy" "scale_3" {
+  name                   = "scale-to-3"
   autoscaling_group_name = aws_autoscaling_group.scale.name
   adjustment_type        = "ExactCapacity"
   policy_type            = "SimpleScaling"
