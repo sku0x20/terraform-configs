522bfc9eb7ca0c30801d79da6aa91c6533636f4f
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-13 17:57:48 +0530

    add scale to 0

--- platform/runner/autoscaling_policy.tf
+++ platform/runner/autoscaling_policy.tf
@@ -8,3 +8,12 @@ resource "aws_autoscaling_policy" "scale" {
 
   scaling_adjustment = 3
 }
+
+resource "aws_autoscaling_policy" "scale_0" {
+  name                   = "scale-to-0"
+  autoscaling_group_name = aws_autoscaling_group.scale.name
+  adjustment_type        = "ExactCapacity"
+  policy_type            = "SimpleScaling"
+
+  scaling_adjustment = 0
+}
