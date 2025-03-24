5305dc48c5c630607c868ed1292e66949596aee7
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-25 00:43:00 +0530

    add varaibles for server

---
+++ platform/runner/server/variables.tf
@@ -0,0 +1,21 @@
+
+variable "name" {
+}
+
+variable "vpc-id" {
+}
+
+variable "subnet_private" {
+}
+
+variable "subnet_public" {
+}
+
+variable "ami" {
+}
+
+variable "key_name" {
+}
+
+variable "instance_type" {
+}
