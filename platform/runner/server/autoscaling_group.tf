bff835114fbebe77603d9954e6e4c156e61ddbf2
siddhant <siddhantkumarupmanyu@gmail.com>
2025-03-25 01:03:44 +0530

    add server autoscaling group

---
+++ platform/runner/server/autoscaling_group.tf
@@ -0,0 +1,29 @@
+
+
+resource "aws_autoscaling_group" "server" {
+
+  name_prefix = var.name
+
+  max_size = 5
+  min_size = 3
+
+  launch_template {
+    id      = aws_launch_template.server.id
+    version = aws_launch_template.server.latest_version
+  }
+
+  vpc_zone_identifier = [var.subnet_private]
+
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+
+  instance_maintenance_policy {
+    min_healthy_percentage = 100
+    max_healthy_percentage = 120
+  }
+
+  instance_refresh {
+    strategy = "rolling"
+  }
+
+}
