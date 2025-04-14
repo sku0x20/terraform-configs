e1c5fcc63806d407438340edc5fc6f87ee361c76
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-14 15:33:00 +0530

    add alb

---
+++ platform/runner/server/alb.tf
@@ -0,0 +1,29 @@
+
+
+resource "aws_lb" "server_alb" {
+  load_balancer_type = "application"
+  internal           = false
+
+  subnets = [var.subnet_public]
+
+  tags = {
+    Name = "${var.name}-alb"
+  }
+}
+
+resource "aws_lb_listener" "server_nomad_listener" {
+  load_balancer_arn = aws_lb.server_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.server_nomad_ui.arn
+    type             = "forward"
+  }
+}
+
+
+resource "aws_lb_target_group" "server_nomad_ui" {
+  port     = 4646
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
