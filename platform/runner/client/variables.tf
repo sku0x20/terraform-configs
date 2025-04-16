937fd1f6a609806d4d0183e34ea351fe20cd1ac7
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-16 15:14:32 +0530

    init client module

---
+++ platform/runner/client/variables.tf
@@ -0,0 +1,28 @@
+
+variable "vpc_id" {
+}
+
+variable "subnet_a" {
+}
+
+variable "subnet_public" {
+}
+
+variable "subnet_public_b" {
+}
+
+variable "ami" {
+}
+
+variable "key_name" {
+}
+
+variable "instance_type" {
+}
+
+variable "name" {
+}
+
+variable "root_block_size" {
+  default = 10
+}
--- platform/runner/main.tf
+++ platform/runner/main.tf
@@ -12,3 +12,15 @@ module "server" {
   name = "${var.name}-server"
 }
 
+
+module "client" {
+  source = "./client"
+  vpc_id = var.vpc_id
+  subnet_a = var.subnet_a
+  subnet_public = var.subnet_public
+  subnet_public_b = var.subnet_public_b
+  ami = var.ami
+  key_name = var.key_name
+  instance_type = var.instance_type
+  name = "${var.name}-server"
+}
