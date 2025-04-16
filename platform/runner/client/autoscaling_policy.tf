8fe74b6f39a86fe3bb2030e7d8fcb3f97d6ca1bc
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-16 15:36:07 +0530

    add scaling

--- platform/runner/client/autoscaling_policy.tf
+++ platform/runner/client/autoscaling_policy.tf
@@ -1,17 +1,17 @@
 
 
-resource "aws_autoscaling_policy" "scale_3" {
+resource "aws_autoscaling_policy" "client_scale_3" {
   name                   = "scale-to-3"
-  autoscaling_group_name = aws_autoscaling_group.scale.name
+  autoscaling_group_name = aws_autoscaling_group.client.name
   adjustment_type        = "ExactCapacity"
   policy_type            = "SimpleScaling"
 
   scaling_adjustment = 3
 }
 
-resource "aws_autoscaling_policy" "scale_0" {
+resource "aws_autoscaling_policy" "client_scale_0" {
   name                   = "scale-to-0"
-  autoscaling_group_name = aws_autoscaling_group.scale.name
+  autoscaling_group_name = aws_autoscaling_group.client.name
   adjustment_type        = "ExactCapacity"
   policy_type            = "SimpleScaling"
 
