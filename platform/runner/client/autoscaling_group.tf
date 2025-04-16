3b57b50db7eaf38fd27ef736acf903927927920d
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-16 15:35:26 +0530

    add nlb

--- platform/runner/client/autoscaling_group.tf
+++ platform/runner/client/autoscaling_group.tf
@@ -29,10 +29,10 @@ resource "aws_autoscaling_group" "client" {
 
 }
 
-resource "aws_autoscaling_traffic_source_attachment" "client_scale_alb" {
+resource "aws_autoscaling_traffic_source_attachment" "client_scale_nlb" {
   autoscaling_group_name = aws_autoscaling_group.client.id
   traffic_source {
-    identifier = aws_lb_target_group.client_nomad_ui.arn
+    identifier = aws_lb_target_group.client.arn
     type = "elbv2"
   }
 }
---
+++ platform/runner/client/nlb.tf
@@ -0,0 +1,45 @@
+
+
+resource "aws_eip" "client_nlb_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.name}-nlb"
+  }
+}
+
+resource "aws_lb" "client_nlb" {
+  load_balancer_type = "network"
+  internal           = false
+
+  subnet_mapping {
+    subnet_id     = var.subnet_public
+    allocation_id = aws_eip.nlb_eip.id
+  }
+
+  tags = {
+    Name = "${var.name}-nlb"
+  }
+
+}
+
+resource "aws_lb_listener" "client_nlb_listener_http" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port = 80
+  protocol = "TCP_UDP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.client_nlb_tg.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group" "client_nlb_tg" {
+  port               = 8080
+  protocol           = "TCP_UDP"
+  preserve_client_ip = true
+  vpc_id             = var.vpc_id
+
+  health_check {
+    path = "/v1/agent/health"
+  }
+}
